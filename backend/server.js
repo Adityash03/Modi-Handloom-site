9b120526140c613a366071d5bcf7491c70e87925
Aditya <adityash6503@gmail.com>
2026-09-05 06:33:51 +0530

    Fix product image server URL

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://modi-handloom-site.vercel.app/',
   })
 );
 app.use(express.json());
