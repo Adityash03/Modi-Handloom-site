315579865c4e717d00d5f5fbd08249c385366fe1
Aditya <adityash6503@gmail.com>
2026-09-05 05:43:33 +0530

    Configure CORS for frontend access

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from "cors";
 
 dotenv.config();
 connectDB();
@@ -17,6 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 
 app.use(
   cors({
